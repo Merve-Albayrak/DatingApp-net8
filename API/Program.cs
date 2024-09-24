a0eba7f7f93b4fcd5ca1b6313025139fa8ccd227
Merve Albayrak <mervealbayrak8@gmail.com>
2024-09-24 23:22:52 +0300

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<DataContext>(opt=>{
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,10 +23,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());  
 
 app.UseAuthorization();
+
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+
+  title = 'DatingApp';
+
+  http=inject(HttpClient);
+  users:any;
+  //constructor(private httpClient: HttpClient){}
+  ngOnInit(): void {
+   this.http.get('https://localhost:5001/api/Users').subscribe(
+
+    {
+      next:response=>this.users=response,
+      error:error=>console.error(error),
+      complete:()=>console.log('Request completed')
+    }
+   )
+  }
+}
+
+
